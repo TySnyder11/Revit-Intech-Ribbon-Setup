4281d8fc6bda55527fe756e4bb2020c1806feb27
tsnyder <tyrysny@gmail.com>
2025-06-25 13:23:15 -0700

    Reapply "Adds a new button which you select elements then it pulls common text parameters then you can find and replace part of the text."
    This reverts commit f1e9e61b5e20b3bac182d0a6bdba8b51b7273b74.

---
+++ Find&Replace/MainF&R.cs
@@ -0,0 +1,101 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TitleBlockSetup.Find_Replace;
+
+namespace Intech
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class MainFindandReplace : IExternalCommand
+    {
+        static Document doc = null;
+        static UIDocument uidoc = null;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            uidoc = commandData.Application.ActiveUIDocument;
+            doc = uidoc.Document;
+
+            FindAndReplaceUI findAndReplaceUI = new FindAndReplaceUI();
+            findAndReplaceUI.ShowDialog();
+
+            return Result.Succeeded;
+        }
+
+        public static List<Element> GetSelectedElements()
+        {
+            List<Reference> s = uidoc.Selection.GetReferences().ToList();
+
+            List<Element> elems = new List<Element>();
+            foreach (Reference r in s)
+            {
+                elems.Add(doc.GetElement(r));
+            }
+            return elems;
+        }
+
+        public static List<string> GetCommonTextParameters(List<Element> elements)
+        {
+            if (elements == null || elements.Count == 0)
+                return new List<string>();
+
+            // Start with the text parameters of the first element
+            var commonParams = new HashSet<string>(
+                elements[0].Parameters
+                    .Cast<Parameter>()
+                    .Where(p => p.StorageType == StorageType.String && p.HasValue)
+                    .Select(p => p.Definition.Name)
+            );
+
+            // Intersect with the rest of the elements
+            foreach (var elem in elements.Skip(1))
+            {
+                var currentParams = new HashSet<string>(
+                    elem.Parameters
+                        .Cast<Parameter>()
+                        .Where(p => p.StorageType == StorageType.String && p.HasValue)
+                        .Select(p => p.Definition.Name)
+                );
+
+                commonParams.IntersectWith(currentParams);
+            }
+
+            return commonParams.ToList();
+        }
+
+        public static bool Replace(List<Element> elements, string parameterName, string findText, string replaceText)
+        {
+            if (elements == null || elements.Count == 0 || string.IsNullOrEmpty(parameterName))
+                return false;
+
+            using (Transaction trans = new Transaction(doc, "Find and Replace, " + parameterName))
+            {
+                trans.Start();
+                foreach (Element e in elements)
+                {
+                    Parameter p = e.LookupParameter(parameterName);
+                    if (p != null)
+                    {
+                        string val = p.AsString();
+                        string newVal = string.Empty;
+                        if (string.IsNullOrEmpty(findText))
+                        {
+                            newVal = val + replaceText;
+                        }
+                        else
+                        {
+                            newVal = val.Replace(findText, replaceText);
+                        }
+                        p.Set(newVal);
+                    }
+                }
+                trans.Commit();
+            }
+
+            return true;
+        }
+    }
+}
---
+++ TemplateCS.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Intech
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class temp : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
